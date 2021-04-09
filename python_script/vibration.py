c86bcd76dea6606c82a35a6959418b05e380b7c7
Félix Manson <felix.manson@icloud.com>
2021-04-09 14:14:06 +0200

    Ajout de vibration.py

---
+++ python_script/vibration.py
@@ -0,0 +1,41 @@
+import pifacedigitalio #lib pour la carte de relais
+import sys
+import time
+
+
+pfd= pifacedigitalio.PiFaceDigital()
+
+in1 = sys.stdin
+
+def motvibron ():
+    pfd.relays[1].turn_on()
+
+def motvibroff ():
+    pfd.relays[1].turn_off()
+
+
+
+while True : #boucle principale
+    while True :
+        if pfd.output_pins[1].value == 1:
+            motvibroff()
+            time.sleep(1)
+        else :
+            time.sleep(1)
+
+        if in1.read() =='Benne_pleine' :
+            break
+
+    while True :
+        t= time.time()
+        if pfd.output_pins[1].value == 0:
+            motvibron()
+            time.sleep(1)
+        else :
+            time.sleep(1)
+        t2 = time.time()
+        if t2 => t + 30 :
+            print('Benne_pleine')
+
+        if in1.read() =='Benne_non_pleine' :
+            break
