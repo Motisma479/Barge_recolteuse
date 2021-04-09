913bbfafc110b94c490adbfc10da1cbc72fa23ba
Félix Manson <felix.manson@icloud.com>
2021-04-09 09:36:12 +0200

    Ajout de autorefresh.py

---
+++ python_script/autorefresh.py
@@ -0,0 +1,21 @@
+import pifacedigitalio
+from sys import stdin
+import Time
+#libairies
+
+pfd= pifacedigitalio.PiFaceDigital()
+
+in1 = sys.stdin #input depuis nodered
+
+def checkstatus (): #fonction pour regarder l'état des relais
+    print ('relay1' + str(pfd.output_pins[0].value))
+    print ('relay2' + str(pfd.output_pins[1].value))
+
+
+while True : #boucle principale
+    while line in in1 =! 'refreshoff':
+        checkstatus()
+        Time.sleep(1000) #délais de 1000ms
+    while line in in1 =! 'refreshon' :
+        Time.sleep(1000)
+        
